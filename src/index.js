9cf73e0648215ce7825ca93dc0b8a61ff9337b80
ELP ITProjects <elpit@georgetown.edu>
2025-08-24 10:47:16 -0500

    Add validation and error handling

--- src/index.js
+++ src/index.js
@@ -1,40 +1,54 @@
 import { Hono } from 'hono'
-import { createOpenAI, openai } from '@ai-sdk/openai';
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+import { createOpenAI, openai } from '@ai-sdk/openai'
 import { generateText } from 'ai'
+
+const schema = z.object({
+  key: z.string(),
+  imageURL: z.string()
+})
 
 const app = new Hono()
 
-app.post('/generate-alt-text', async (c) => {
-  const body = await c.req.json()
+app.post('/generate-alt-text', zValidator('json', schema), async (c) => {
+  const body = await c.req.valid('json')
 
   // use user-provided key
   const openai = createOpenAI({
     apiKey: body.key
   })
 
-  const result = await generateText({
-    // most efficient and affordable model as of 08/2025
-    model: openai('gpt-5-nano'),
-    messages: [
-      {
-        role: 'user',
-        content: [
-          {
-            type: 'text',
-            text: 'describe the image in 10 words or less',
-          },
-          {
-            type: 'image',
-            image: new URL(
-              body.imageURL,
-            ),
-          },
-        ],
-      },
-    ],
-  });
+  let result;
 
-  return c.json(result);
+  try {
+    result = await generateText({
+      // most efficient and affordable model as of 08/2025
+      model: openai('gpt-5-nano'),
+      messages: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: 'describe the image in 10 words or less',
+            },
+            {
+              type: 'image',
+              image: new URL(
+                body.imageURL,
+              ),
+            },
+          ],
+        },
+      ],
+    });
+  } catch(error) {
+    // handle API access errors (e.g. quota exceeded)
+    return c.json(error, 429)
+  }
+
+  return c.json(result)
 })
 
 export default app
